d8a4fc8eb836d996a29c4dbf8d5133351bd847dc
Ambika Mohan <ambika.mohan@stratforge.com>
2023-06-07 13:27:21 +0530

    customer_type

--- models/generated/newtransform/newsql.sql
+++ models/generated/newtransform/newsql.sql
@@ -1,24 +1,17 @@
+
+{% set cust_domains = ["hpe.com", "arubanetworks.com", "hpe.hr","hpecds.com","jpn.hpe.com","hpecds.com"] %}
+
 select
-    case_number,
-    case_origin,
-    account_name,
-    federal_case,
-    exclusion_flag,
-    previous_owner,
-    product_number,
-    requestor_lang,
-    escalation_date,
-    entitlement_summary,
-    part_arrived_ontime,
-    survey_received_date,
-    esc_addressed_datetime,
-    closeddate_aibyte_transform,
-    resolution_provided_satisfactory,
-    _airbyte_ab_id,
-    _airbyte_emitted_at,
-    {{ current_timestamp() }} as _airbyte_normalized_at,
-    _airbyte_date_convertor_hashid
+customer_domain,
+{% for customer_domain in cust_domains %}
+(case when customer_domain = '{{customer_domain}}' then 'Internal' end) as customer_type,
+{% endfor %}
+_airbyte_ab_id,
+_airbyte_emitted_at,
+{{ current_timestamp() }} as _airbyte_normalized_at,
+_airbyte_date_convertor_hashid
 from {{ ref('date_convertor_ab3') }}
 -- date_convertor from {{ source('public', '_airbyte_raw_date_convertor') }}
 where 1 = 1
+
 
