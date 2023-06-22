4aac050bd1406d9af2ba66636eebabe0618bc13c
Ambika Mohan <ambika.mohan@stratforge.com>
2023-06-22 13:49:33 +0530

    new one

--- models/generated/newtransform/newsql.sql
+++ models/generated/newtransform/newsql.sql
@@ -30,11 +30,7 @@ _airbyte_ab_id,
 _airbyte_emitted_at,
 {{ current_timestamp() }} as _airbyte_normalized_at,
 _airbyte_date_convertor_hashid,
-{% if case_contact_email != NULL %}
-    substring(case_contact_email, POSITION('@' IN case_contact_email) + 1) AS {{ cust_dom }}
-{% else %}
-    case_contact_email AS {{ cust_dom }}
-{% endif %}
+substring(case_contact_email, POSITION('@' IN case_contact_email) + 1) AS {{ cust_dom }}
 from {{ ref('date_convertor_ab3') }}
 -- date_convertor from {{ source('public', '_airbyte_raw_date_convertor') }}
 where 1 = 1
