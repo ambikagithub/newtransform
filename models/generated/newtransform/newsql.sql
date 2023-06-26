dbf25597ffbcfd5b922ed67c983095cc435767ca
Ambika Mohan <ambika.mohan@stratforge.com>
2023-06-26 14:23:06 +0530

    new now

--- models/generated/newtransform/newsql.sql
+++ models/generated/newtransform/newsql.sql
@@ -27,7 +27,7 @@
 select
 coalesce(customer_domain,'no_value') as customer_domain,
 substring(case_contact_email, POSITION('@' IN case_contact_email) + 1) AS {{ cust_dom }},
-{% if cust_dom == 'hpe.com' %}
+{% if new_customer_domain == "hpe.com" %}
     'internal'    customer_type
 {% else %}
     'external'   customer_type
